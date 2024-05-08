acefae3fa0b98670cd523380e5416f1a098d5ded
Mohammad Afreen Aqthar <110152840+md-afreen-aqthar@users.noreply.github.com>
2024-05-08 15:34:42 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Home from './features/pages/Home';
+import LoginPage from './features/pages/LoginPage';
+import SignupPage from './features/pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+          <RouterProvider router={router} />
+    </div>
+  );
+}
+export default App;
