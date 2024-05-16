e4b089616fdd4c18ba25acd1b76ae80d75e14384
Mohammad Afreen Aqthar <110152840+md-afreen-aqthar@users.noreply.github.com>
2024-05-16 13:32:01 +0530

    product_Details

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,17 +1,27 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts ,fetchProductsByFilters, fetchBrands,fetchCategories } from './productAPI';
+import { fetchAllProducts ,fetchProductsByFilters, fetchBrands,fetchCategories,fetchProductById} from './productAPI';
 
 const initialState = {
   products: [],
   status: 'idle',
   categories: [],
   brands: [],
-  totalItems:0
+  totalItems:0,
+  selectProduct:null
 };
 export const fetchAllProductsAsync = createAsyncThunk(
   'product/fetchAllProducts',
   async () => {
     const response = await fetchAllProducts();
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchAllProductByIdAsync = createAsyncThunk(
+  'product/fetchProductById',
+  async (id) => {
+    const response = await fetchProductById(id);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -83,12 +93,19 @@ export const productSlice = createSlice({
         state.status = 'idle';
         state.categories = action.payload;
       })
+      .addCase(fetchAllProductByIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllProductByIdAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.selectdProduct = action.payload;
+      })
   },
 });
 export const { increment } = productSlice.actions;
 export const selectAllProducts = (state) => state.product.products;
 export const selectBrands = (state) => state.product.brands;
 export const selectCategories = (state) => state.product.categories;
-
+export const selectProductById = (state) => state.product.selectedProduct;
 export const selectTotalItems = (state) => state.product.totalItems;
 export default productSlice.reducer;
